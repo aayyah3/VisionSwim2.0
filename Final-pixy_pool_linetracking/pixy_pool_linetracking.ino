cc58c9d88982c4ed9c54a7b8ea4afdd3ef9ce887
aayyah3 <akshayaayyah@gmail.com>
2025-11-03 15:48:29 -0600

    Update pixy_pool_linetracking.ino

--- Final-pixy_pool_linetracking/pixy_pool_linetracking.ino
+++ Final-pixy_pool_linetracking/pixy_pool_linetracking.ino
@@ -14,50 +14,57 @@
 //
 
 #include <Pixy2.h>
-#define PIXY_MAX_X 78
-#define TURN_LEFT_PIN A0
+//max x coordinate
+#define PIXY_MAX_X 78 
+
+//Arduino pins that will be used to control motors or actuators for turning.
+#define TURN_LEFT_PIN A0 
 #define TURN_RIGHT_PIN A2
 
 Pixy2 pixy;
-int threshold = 20;
+int threshold = 20; // A “dead zone” from the left or right side of the camera’s view.
 
 void setup()
 {
-  Serial.begin(115200);
+  Serial.begin(115200); //serial communication so the Arduino can print debug info to the computer
   Serial.print("Starting...\n");
 
-  pixy.init();
+  pixy.init(); //Initializes the Pixy camera so it’s ready to detect lines.
   // change to the line_tracking program.  Note, changeProg can use partial strings, so for example,
   // you can change to the line_tracking program by calling changeProg("line") instead of the whole
   // string changeProg("line_tracking")
-  Serial.println(pixy.changeProg("line"));
+  Serial.println(pixy.changeProg("line")); //Tells the Pixy camera to switch to line tracking mode.
 }
 
 void loop()
 {
-  pixy.line.getAllFeatures();
+  pixy.line.getAllFeatures(); //Requests all the line vectors Pixy sees
 
-  if (pixy.line.numVectors){
+  if (pixy.line.numVectors){ 
     Serial.println(pixy.line.numVectors);
-    if(pixy.line.numVectors > 1){
-      analogWrite(TURN_LEFT_PIN, 255);
+    if(pixy.line.numVectors > 1){ //If Pixy sees more than one line vector → the robot assumes it reached a junction or intersection
+      //stop or turn-around
+      analogWrite(TURN_LEFT_PIN, 255); 
       analogWrite(TURN_RIGHT_PIN, 255);
       Serial.println("Turn around");
     }
     else if (pixy.line.vectors->m_x1 < threshold) {
+      //turn left
       analogWrite(TURN_LEFT_PIN, 255);
       analogWrite(TURN_RIGHT_PIN, 0);
       Serial.println("Turn Left"); 
     } else if (pixy.line.vectors->m_x1 > PIXY_MAX_X - threshold) {
+      //turn right
       analogWrite(TURN_LEFT_PIN, 0);
       analogWrite(TURN_RIGHT_PIN, 255);
       Serial.println("Turn Right");
     } else {
+      //go straight
       analogWrite(TURN_LEFT_PIN, 0);
       analogWrite(TURN_RIGHT_PIN, 0);
       Serial.println("Go Straight");
     }
-     
+    //nothing detected 
   } else {
     analogWrite(TURN_LEFT_PIN, 0);
     analogWrite(TURN_RIGHT_PIN, 0);
@@ -66,3 +73,4 @@ void loop()
 
   delay(100);
 }
+

a72f87e81f2b08cd08bc4c2fab78d1212ddaa853
ryansullivan1 <112103211+ryansullivan1@users.noreply.github.com>
2026-02-11 19:06:46 -0600

    Change RGB value to average of 3 points on the line

--- Final-pixy_pool_linetracking/experimental.ino
+++ Final-pixy_pool_linetracking/experimental.ino
@@ -12,12 +12,12 @@
 #define GROUND_PIN_1 A1
 
 // Pool Lane Color Signature (Tune these!)
-const uint8_t POOL_R = 28;
-const uint8_t POOL_G = 71;
-const uint8_t POOL_B = 97;
+const uint8_t POOL_R = 27;
+const uint8_t POOL_G = 53;
+const uint8_t POOL_B = 60;
 
 // Optimization Constants
-const int MIN_LINE_LENGTH_SQ = 400; // Vector must be ~20 pixels long (20^2)
+const int MIN_LINE_LENGTH_SQ = 0; // Vector must be ~20 pixels long (20^2)
 const int COLOR_THRESHOLD = 4000;   // Distance threshold for "Blue" check
 const int REQUIRED_FRAMES = 3;      // Reduced from 5 for faster response
 
@@ -50,6 +50,8 @@ void setup() {
   pinMode(TURN_RIGHT_PIN, OUTPUT);
   analogWrite(GROUND_PIN_0, 0);
   analogWrite(GROUND_PIN_1, 0);
+  pixy.setCameraBrightness(50);        // Try lower values
+
 }
 
 void loop() {
@@ -69,12 +71,23 @@ void loop() {
 
       if (lenSq >= MIN_LINE_LENGTH_SQ) {
         // Check Color (Flowchart: Is vector blue?)
-        uint8_t r, g, b;
+        uint8_t r_mid, g_mid, b_mid, r_st, g_st, b_st, r_end, g_end, b_end;
         int sx = (pixy.line.vectors[i].m_x0 + pixy.line.vectors[i].m_x1) / 2;
         int sy = (pixy.line.vectors[i].m_y0 + pixy.line.vectors[i].m_y1) / 2;
-        pixy.video.getRGB(sx, sy, &r, &g, &b);
+        
+        pixy.video.getRGB(sx, sy, &r_mid, &g_mid, &b_mid);
+        pixy.video.getRGB(pixy.line.vectors[i].m_x0, pixy.line.vectors[i].m_y0, &r_st, &g_st, &b_st);
+        pixy.video.getRGB(pixy.line.vectors[i].m_x1, pixy.line.vectors[i].m_y1, &r_end, &g_end, &b_end);
 
-        long dist = pow(r-POOL_R, 2) + pow(g-POOL_G, 2) + pow(b-POOL_B, 2);
+        int r_avg = (r_mid + r_st + r_end)/3;
+        int g_avg = (r_mid + r_st + r_end)/3;
+        int b_avg = (r_mid + r_st + r_end)/3;
+        
+        Serial.println(
+              String("Vector ") + i +
+              " RGB value: " + r_avg + "," + g_avg + "," + b_avg + "|| m index: " + pixy.line.vectors[i].m_index
+            );        
+        long dist = pow(r_avg-POOL_R, 2) + pow(g_avg-POOL_G, 2) + pow(b_avg-POOL_B, 2);
 
         if (dist < minDist) {
           minDist = dist;
@@ -85,6 +98,9 @@ void loop() {
 
   // STEP 2: Decide Direction (Flowchart: Check Position)
   if (best_idx != -1) {
+    Serial.println(
+              String("SELECTED VECTOR: ") + best_idx
+            );
     int lineCenterX = (pixy.line.vectors[best_idx].m_x0 + pixy.line.vectors[best_idx].m_x1) / 2;
     if (lineCenterX < threshold) currentDir = DIR_LEFT;
     else if (lineCenterX > PIXY_MAX_X - threshold) currentDir = DIR_RIGHT;
@@ -100,5 +116,5 @@ void loop() {
     signalDirection(confirmedDir);
   }
 
-  delay(50); // Increased frequency for better tracking responsiveness
+  delay(500); // Increased frequency for better tracking responsiveness
 }
